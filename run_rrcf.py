42a3d178bb5f15eac3c4f55548afcb59dafba53d
sjpberge <57839199+sjpberge@users.noreply.github.com>
2020-12-15 16:19:23 +0100

    Minor loop changes

--- run_rrcf.py
+++ run_rrcf.py
@@ -14,25 +14,34 @@ import pyshark
 # Load the data
 capture = pyshark.FileCapture('./2019 Singapore ICS data/Dec2019_00000_20191206100500.pcap')
 packet_list = []
-for i, packet in enumerate(capture):
-    if i == 10000:
-        break
-    packet_list.append(packet)
+# for i, packet in enumerate(capture):
+#     if i == 10000:
+#         break
+#     packet_list.append(packet)
 
 # Relevant features
+# layers_dict = {'ETH': [{'dst_lg': []}, {'lg': []}, {'dst_ig': []}, {'ig': []}, {'src_lg': []}, {'src_ig': []}],
+#                'VLAN': [{'priority': []}, {'dei': []}, {'id': []}],
+#                'IP': [{'hdr_len': []}, {'len': []}, {'flags_rb': []}, {'flags_df': []}, {'flags_mf': []}, {'frag_offset': []}, {'ttl': []}, {'proto': []}, {'checksum_status': []}],
+#                'TCP': [{'stream':[]}, {'len':[]}, {'seq':[]}, {'nxtseq':[]}, {'ack':[]},
+#                 {'hdr_len':[]}, {'flags_res':[]}, {'flags_ns':[]}, {'flags_cwr':[]}, {'flags_ecn':[]}, {'flags_urg':[]}, {'flags_ack':[]},
+#                 {'flags_push':[]}, {'flags_reset':[]}, {'flags_syn':[]}, {'flags_fin':[]}, {'checksum_status':[]}, {'urgent_pointer':[]}],
+#                'UDP':[{'length':[]}, {'checksum_status':[]}, {'stream':[]}, {'time_relative':[]}, {'time_delta':[]}]}
+
 layers_dict = {'ETH': [{'dst_lg': []}, {'lg': []}, {'dst_ig': []}, {'ig': []}, {'src_lg': []}, {'src_ig': []}],
-               'VLAN': [{'priority': []}, {'dei': []}, {'id': []}],
-               'IP': [{'hdr_len': []}, {'len': []}, {'flags_rb': []}, {'flags_df': []}, {'flags_mf': []}, {'frag_offset': []}, {'ttl': []}, {'proto': []}, {'checksum_status': []}],
-               'TCP': [{'stream':[]}, {'len':[]}, {'seq':[]}, {'nxtseq':[]}, {'ack':[]},
-                {'hdr_len':[]}, {'flags_res':[]}, {'flags_ns':[]}, {'flags_cwr':[]}, {'flags_ecn':[]}, {'flags_urg':[]}, {'flags_ack':[]},
-                {'flags_push':[]}, {'flags_reset':[]}, {'flags_syn':[]}, {'flags_fin':[]}, {'checksum_status':[]}, {'urgent_pointer':[]}],
-               'UDP':[{'length':[]}, {'checksum_status':[]}, {'stream':[]}, {'time_relative':[]}, {'time_delta':[]}]}
+               'VLAN': [{'priority': []}, {'dei': []}],
+               'IP': [{'hdr_len': []}, {'len': []},
+                      {'ttl': []}, {'proto': []}, {'checksum_status': []}],
+               'TCP': [{'stream': []}, {'len': []}, {'seq': []}, {'nxtseq': []},
+                       {'hdr_len': []},
+                       {'checksum_status': []}, {'urgent_pointer': []}],
+               'UDP': [{'length': []}, {'checksum_status': []}, {'stream': []}]}
 
 # Set tree parameters
 num_trees = 40
-shingle_size = 4
-tree_size = 256
-num_feats = 41
+shingle_size = 2
+tree_size = 128
+num_feats = 23
 
 # Create a forest of empty trees
 forest = []
@@ -48,10 +57,13 @@ avg_codisp = {}
 
 # For each shingle...
 # Need to account for NA values
-for index in range(len(packet_list) - shingle_size):
-
+# for index in range(len(packet_list) - shingle_size):
+for index in range(1000):
     print(index)
-    window = packet_list[index:index + shingle_size]
+    # window = []
+    # for i in range(shingle_size):
+    #     window.append(capture[index + i])
+    # window = capture[index:index + shingle_size]
     updated_window = np.empty((shingle_size, num_feats))
 
     for j in range(shingle_size):
@@ -62,7 +74,8 @@ for index in range(len(packet_list) - shingle_size):
                 try:
                     attribute_name = list(attribute.keys())[0]
                     # layers_dict[layer_key][k][attribute_name].append(getattr(packet_list[j][layer_key], list(attribute.keys())[0]))
-                    row.append(getattr(window[j][layer_key], list(attribute.keys())[0]))
+                    # row.append(getattr(window[j][layer_key], list(attribute.keys())[0]))
+                    row.append(getattr(capture[index + j][layer_key], list(attribute.keys())[0]))
                     # column_names[attribute].append(val)
                 except:
                     attribute_name = list(attribute.keys())[0]
@@ -72,6 +85,20 @@ for index in range(len(packet_list) - shingle_size):
         updated_window[j, :] = row
 
     point = np.nan_to_num(updated_window)
+
+    # Do min-max norm here here
+    # https://stats.stackexchange.com/questions/441342/same-value-of-min-and-max-in-min-max-normalisation
+    if index == 0:
+        window_max = point.max(axis=0).reshape(1, -1)
+        window_min = point.min(axis=0).reshape(1, -1)
+    else:
+        window_max = np.r_[point, window_max].max(axis=0).reshape(1, -1)
+        window_min = np.r_[point, window_min].min(axis=0).reshape(1, -1)
+
+    difference = window_max - window_min
+    difference[difference == 0] = 1
+    point = (point - window_min) / difference
+
     # print(point)
     # Point shape is shingle_size x dimensions
     # For each tree in the forest...
@@ -80,20 +107,8 @@ for index in range(len(packet_list) - shingle_size):
         if len(tree.leaves) > tree_size:
             # Drop the oldest point (FIFO)
             tree.forget_point(index - tree_size)
-        #
-        # Do min-max norm here here
-        # https://stats.stackexchange.com/questions/441342/same-value-of-min-and-max-in-min-max-normalisation
-        if index == 0:
-            window_max = point.max(axis=0).reshape(1, -1)
-            window_min = point.min(axis=0).reshape(1, -1)
-        else:
-            window_max = np.r_[point, window_max].max(axis=0).reshape(1, -1)
-            window_min = np.r_[point, window_min].min(axis=0).reshape(1, -1)
 
-        difference = window_max - window_min
-        difference[difference==0] = 1
-        point = (point - window_min) / difference
-        #
+
         # Insert the new point into the tree
         tree.insert_point(point, index=index)
         # Compute codisp on the new point...
@@ -102,7 +117,7 @@ for index in range(len(packet_list) - shingle_size):
         if not index in avg_codisp:
             avg_codisp[index] = 0
         avg_codisp[index] += new_codisp / num_trees
-    print(avg_codisp[index])
+    # print(avg_codisp[index])
 
 fig, ax1 = plt.subplots(figsize=(10, 5))
 
